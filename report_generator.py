abee5a9c2f1e6e242115f2554d59a5d20baab3d9
Javier Alejandro Oramas López <javiale2000@gmail.com>
2023-08-29 14:49:59 -0400

    ix minor bug for alpha ratio not being displayed correctly in summary

--- report_generator.py
+++ report_generator.py
@@ -28,13 +28,13 @@ def generate_excel_summary(raw_report_data, excel_file_path="summary.xlsx"):
         else:
             summary_counts_legibility['80>'] += 1
             
-        if legible_ratio < 0.2:
+        if alpha_ratio < 0.2:
             summary_counts_alphanumerical['< 20'] += 1
-        elif legible_ratio < 0.4:
+        elif alpha_ratio < 0.4:
             summary_counts_alphanumerical['20-40'] += 1
-        elif legible_ratio < 0.6:
+        elif alpha_ratio < 0.6:
             summary_counts_alphanumerical['40-60'] += 1
-        elif legible_ratio < 0.8:
+        elif alpha_ratio < 0.8:
             summary_counts_alphanumerical['60-80'] += 1
         else:
             summary_counts_alphanumerical['80>'] += 1
